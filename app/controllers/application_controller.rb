6beee9738a6c493749ed8192e5130a346b429921
cwen-booth <cwen@chicagobooth.edu>
2023-01-24 05:26:20 +0000

    HW Complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,27 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+  def homepage
+    render({ :template => "game_templates/homepage.html.erb" })
+  end
 
+  def play_rock
+    @comp_move = ["rock", "paper", "scissors"].sample
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+    render({ :template => "game_templates/user_rock.html.erb", :layout => "wrapper.html.erb" })
+  end
+
+  def play_paper
+    render({ :template => "game_templates/user_paper.html.erb", :layout => "wrapper.html.erb" })
+  end
+
+  def play_scissors
+    render({ :template => "game_templates/user_scissors.html.erb", :layout => "wrapper.html.erb" })
+  end
 end
